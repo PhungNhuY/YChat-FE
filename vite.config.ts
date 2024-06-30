8faa38ddff132258a5147a57e639f430c320e258
PhungNhuY <yphung282@gmail.com>
2024-06-30 18:47:27 +0700

    feat: add env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: { mode: string }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: parseInt(process.env.VITE_PORT as string) | 3000,
+    },
+  });
+};
