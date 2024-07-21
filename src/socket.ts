05f28f43480fd71d2b1f8246e8cf9147afd2c010
PhungNhuY <yphung282@gmail.com>
2024-07-22 00:04:26 +0700

    feat: update ws config

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(import.meta.env.VITE_API_URL as string, {
+export const socket = io(import.meta.env.VITE_WS_URL as string, {
   path: '/ws',
   autoConnect: true,
   withCredentials: true,
