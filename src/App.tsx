598ea95969c344afff52ce6cbc56fdc63a44c8b4
PhungNhuY <yphung282@gmail.com>
2024-07-25 23:04:36 +0700

    feat: sider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import './App.css';
 import { AuthProvider } from './providers';
 import Routes from './router';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { ConfigProvider } from 'antd';
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorBgContainer: '#f5f5f5',
+        },
+      }}
+    >
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </ConfigProvider>
   );
 }
 
--- src/layouts/authenticated-layout.tsx
+++ src/layouts/authenticated-layout.tsx
@@ -1,18 +1,69 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth, useSetup } from '../hooks';
+import { Layout, Menu, MenuProps, theme } from 'antd';
+import { useState } from 'react';
+import { CommentOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
+import { IUser } from '../types';
+
+const { Content, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+    label: 'Conversations',
+    key: 'Conversations',
+    icon: <CommentOutlined />,
+  },
+  {
+    label: 'People',
+    key: 'People',
+    icon: <TeamOutlined />,
+  },
+];
 
 export function AuthenticatedLayout() {
-  const [contextHolder] = useSetup();
-  const { user } = useAuth();
-
+  const { user }: { user: IUser } = useAuth();
   if (!user._id) {
     return <Navigate to="/login" />;
   }
 
+  const [collapsed, setCollapsed] = useState(false);
+  const [contextHolder] = useSetup();
+  const { token } = theme.useToken();
+
   return (
     <>
       {contextHolder}
-      <Outlet />;
+      <Layout style={{ height: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+          style={{ background: token.colorBgContainer }}
+          theme="light"
+        >
+          <div className="d-flex flex-column justify-content-between h-100">
+            <Menu
+              theme="light"
+              items={items}
+              defaultSelectedKeys={['Conversations']}
+            />
+            <Menu
+              theme="light"
+              items={[
+                { label: user.name, key: user.name, icon: <UserOutlined /> },
+              ]}
+              defaultSelectedKeys={['Conversations']}
+            />
+          </div>
+        </Sider>
+        <Layout>
+          <Content>
+            <Outlet />
+          </Content>
+        </Layout>
+      </Layout>
     </>
   );
 }
