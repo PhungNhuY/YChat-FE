a46a035ebd2079fca27df52a926257d997fb7cce
PhungNhuY <yphung282@gmail.com>
2024-09-07 23:59:07 +0700

    feat: conversation list

---
+++ src/modules/home/conversation-list/conversation-list.tsx
@@ -0,0 +1,59 @@
+import clsx from 'clsx';
+import styles from './conversation-list.module.css';
+import { IoCreateOutline } from 'react-icons/io5';
+import { Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { FaArrowLeft } from 'react-icons/fa6';
+import { useState } from 'react';
+
+export function ConversationList() {
+  const [isSearching, setIsSearching] = useState(false);
+
+  return (
+    <div className={clsx(styles.conversationList, 'd-flex flex-column')}>
+      <div
+        className={clsx(
+          'd-flex justify-content-between align-items-center',
+          'flex-fixed-size',
+          styles.heading,
+        )}
+      >
+        <span className={clsx(styles.headingTitle)}>Conversations</span>
+        <button className={clsx(styles.newConversationButton)}>
+          <IoCreateOutline
+            size={18}
+            className={clsx(styles.newConversationButtonIcon)}
+          />
+        </button>
+      </div>
+      <div
+        className={clsx(
+          styles.search,
+          'd-flex justify-content-between align-items-center gap-1',
+          'flex-fixed-size',
+        )}
+      >
+        {isSearching && (
+          <button
+            className={clsx(
+              styles.search_backButton,
+              'd-flex justify-content-center align-items-center',
+              'flex-fixed-size',
+            )}
+            onClick={() => setIsSearching(false)}
+          >
+            <FaArrowLeft />
+          </button>
+        )}
+        <Input
+          type="text"
+          className={clsx(styles.searchInput, 'flex-expanding-size')}
+          placeholder="Find on YChat"
+          prefix={<SearchOutlined />}
+          onClick={() => setIsSearching(true)}
+        />
+      </div>
+      <div className={clsx(styles.list, 'flex-expanding-size')}></div>
+    </div>
+  );
+}
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,5 +1,27 @@
-import { Home } from '../modules/home/home';
+import clsx from 'clsx';
+import styles from './home-page.module.css';
+import { ConversationList } from '../modules/home/conversation-list/conversation-list';
 
 export const HomePage = () => {
-  return <Home />;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.mainContent}>
+        <div
+          className={clsx(styles.block, styles.block1)}
+          id="home-conversation-list"
+        >
+          <ConversationList />
+        </div>
+        <div className={clsx(styles.block, styles.block2)} id="home-messages">
+          2
+        </div>
+        <div
+          className={clsx(styles.block, styles.block3)}
+          id="home-conversation-info"
+        >
+          3
+        </div>
+      </div>
+    </div>
+  );
 };
