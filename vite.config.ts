6bc4ea0a41beaacd1e7cf18a7094fa0ddc6aef7a
PhungNhuY <yphung282@gmail.com>
2024-09-11 23:36:46 +0700

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
@@ -7,9 +10,14 @@ export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(), eslint(), tsConfigPaths()],
     server: {
       port: parseInt(process.env.VITE_PORT as string) || 3000,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
   });
 };
