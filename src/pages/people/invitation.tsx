d790d4432ade07ee832d2bbf31a954f9dbfd4bd5
PhungNhuY <yphung282@gmail.com>
2024-12-09 23:25:02 +0700

    feat: update invitation page

--- src/pages/people/invitation.tsx
+++ src/pages/people/invitation.tsx
@@ -2,10 +2,13 @@ import { useDispatch } from 'react-redux';
 import {
   AppDispatch,
   getReceivedRequestThunk,
+  loadMoreReceivedRequestThunk,
   useAppSelector,
 } from '../../store';
 import { useEffect } from 'react';
 import { InvitationCard } from '../../modules/friendship-invitation';
+import { Button, Spin } from 'antd';
+import { CaretDownOutlined } from '@ant-design/icons';
 
 export function PeopleInvitationPage() {
   const receivedRequestState = useAppSelector((state) => state.receivedRequest);
@@ -29,6 +32,22 @@ export function PeopleInvitationPage() {
           </div>
         ))}
       </div>
+      {receivedRequestState.loading && (
+        <div className="w-100 p-4 d-flex justify-content-center">
+          <Spin />
+        </div>
+      )}
+      {!receivedRequestState.loading &&
+        receivedRequestState.page < receivedRequestState.numberOfPages && (
+          <Button
+            className="w-100 mt-3"
+            icon={<CaretDownOutlined />}
+            onClick={() => dispatch(loadMoreReceivedRequestThunk())}
+          >
+            {' '}
+            More
+          </Button>
+        )}
     </div>
   );
 }
