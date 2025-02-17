4bc303fdc911cdad3f410e0a7c96b35e18d93a9f
PhungNhuY <yphung282@gmail.com>
2025-02-17 22:10:27 +0700

    feat: reset redux store when logout

--- src/hooks/use-logout.tsx
+++ src/hooks/use-logout.tsx
@@ -2,12 +2,16 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from './use-auth';
 import { AuthStorageService } from '../services';
 import { logout as logoutApi } from '../services';
+import { useDispatch } from 'react-redux';
+import { resetStore } from '../store';
 
 export function useLogout() {
   const navigate = useNavigate();
   const { setUser } = useAuth();
+  const dispatch = useDispatch();
   const logout = () => {
     logoutApi();
+    dispatch(resetStore());
     AuthStorageService.resetAll();
     setUser({});
     navigate('/login');
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import conversationReducer from './conversations.slice';
 import currentConversationReducer from './current-conversation.slice';
 import receivedRequestReducer from './received-request.slice';
 
-export const store = configureStore({
-  reducer: {
-    conversation: conversationReducer,
-    currentConversation: currentConversationReducer,
-    receivedRequest: receivedRequestReducer,
-  },
+export const RESET_STATE = 'RESET_STATE';
+
+const rootReducer = combineReducers({
+  conversation: conversationReducer,
+  currentConversation: currentConversationReducer,
+  receivedRequest: receivedRequestReducer,
 });
+
+const rootReducerWithReset = (state: any, action: any) => {
+  if (action.type === RESET_STATE) {
+    // reset state
+    return rootReducer(undefined, action);
+  }
+  return rootReducer(state, action);
+};
+
+export const store = configureStore({
+  reducer: rootReducerWithReset,
+});
+
+// reset store action
+export const resetStore = () => ({ type: RESET_STATE });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
